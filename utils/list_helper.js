ad01142697792d33dadff5fb45227a18106c0221
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2018-08-22 12:35:07 +0300

    Tehtävä 4.4
    Valmis.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((count, blog) => {
+    return count + blog.likes
+  },0)
+
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
